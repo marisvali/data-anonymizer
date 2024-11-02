e34b23621badccf3f1af8cfdf06ecd2ecbc9cfd1
Vali Maris <marisvali@yahoo.com>
2024-11-02 15:18:04 +0200

    rename hash to anonymize

--- hash.py
+++ hash.py
@@ -1,9 +1,8 @@
 import random
 import string
 
-class TextHasher:
+class TextAnonymizer:
     def __init__(self):
-        # Load dictionary.
         self.words : list[str] = []
         self.weights : list[int] = []
         with open('words_frequencies.csv') as file:
@@ -12,21 +11,26 @@ class TextHasher:
                 self.words.append(tokens[0])
                 self.weights.append(int(tokens[1]))
 
-    def _hashed_word(self, word: str) -> str:
-        if len(word) == 0:
-            return word
+    def _random_word(self) -> str:
         # Choose a random word based on its frequency as used in the English language.
-        # This creates random speech that retains only the number of words from the original text.
+        return random.choices(population=self.words, weights=self.weights, k=1)[0]
+
+    def anonymize_text(self, text: str) -> str:
+        # Go through the string. Add separators to the anonymized text as they are.
+        # Replace words between separators with random English words based on their frequency.
+        # This creates random text that retains only the number of words from the original text.
         # If the original text uses natural language and is large enough, the randomly generated
         # text should exhibit some properties of the original text, such as:
         # - similar overall text size
-        # - similar number of times a random part of the text is found throughout the whole text
-        return random.choices(population=self.words, weights=self.weights, k=1)[0]
-
-    def hashed_text(self, text: str) -> str:
-        # Go through the string. Add separators to the hashed text as they are.
-        # Transform words between separators by hashing them and then add them to the hashed text.
-        # We consider any non-letter to be a separator. Separators are ignored.
+        # - similar number of times a random part of the text is found throughout the whole text.
+        # The reason for not using another hashing technique is because of the possibility of
+        # deciphering the original text based on the frequency of words in natural language.
+        # For example the most common word in English is 'the' and you can just find the most
+        # common word in the encoded text and assume it will be 'the'.
+        # So if there is any 1-to-1 relationship between a word in the original text and a word
+        # in the encoded text, the encoded text can theoretically be decoded by someone patient
+        # enough.
+        # We consider any non-letter to be a separator.
         h_text = ""
         word = ""
         for c in text:
@@ -36,19 +40,21 @@ class TextHasher:
             else:
                 # We reached a separator.
                 # If we have a word since the last separator, hash it.
-                h_text += self._hashed_word(word)
+                if len(word) > 0:
+                    h_text += self._random_word()
                 # Add the separator to the hashed text.
                 h_text += c
                 # Now, reset the word and let it build up until the next separator.
                 word = ""
         # If the text didn't end with a separator, we might still have a word.
-        h_text += self._hashed_word(word)
+        if len(word) > 0:
+            h_text += self._random_word()
         return h_text
 
 # If this script is running directly, test the function.
 if __name__ == "__main__":
-    hasher = TextHasher()
-    result = hasher.hashed_text(
+    anonymizer = TextAnonymizer()
+    result = anonymizer.anonymize_text(
         "Am pierdut o batistuta, ma bate mamica.\n"
         "And I will strike down upon thee with great vengeance and furious\n"
         "Anger those who attempt to poison and destroy my brothers.\n"
