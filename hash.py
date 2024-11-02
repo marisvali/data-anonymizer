460ab5835a81cb860516384a64132c6942ffd2fb
Vali Maris <marisvali@yahoo.com>
2024-11-02 14:02:02 +0200

    hash by mapping consistently to random english words of similar length

--- hash.py
+++ hash.py
@@ -7,13 +7,36 @@ class TextHasher:
         with open('words_alpha.txt') as file:
             self.all_english_words = [line.strip() for line in file]
 
+        # Create buckets of words of similar length (+/-2 chars).
+        self.english_words_of_length: dict[int, list[str]] = {}
+        for word in self.all_english_words:
+            fixed_length = len(word)
+            # Add to all buckets where it belongs.
+            for var_length in range(fixed_length-2, fixed_length+2):
+                # Protect against things like fixed_length == 1.
+                if var_length > 0:
+                    # Create list if it was never created.
+                    if var_length not in self.english_words_of_length.keys():
+                        self.english_words_of_length[var_length] = []
+                    # Add word to this list.
+                    self.english_words_of_length[var_length].append(word)
+
         self.map: dict[str, str] = {}
 
     def _hashed_word(self, word: str) -> str:
         if len(word) == 0:
             return word
         if word not in self.map:
-            self.map[word] = random.choice(self.all_english_words)
+            # Choose a random word of similar length.
+            # We might encounter words that are too long and don't have a bucket.
+            # In that case, map them to a random word.
+            length = len(word)
+            if length not in self.english_words_of_length.keys():
+                self.map[word] = random.choice(self.all_english_words)
+            else:
+                self.map[word] = random.choice(self.english_words_of_length[length])
+
+
         return self.map[word]
 
     def hashed_text(self, text: str) -> str:
