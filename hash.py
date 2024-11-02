6badfed9dc1e41fa6bcaf99232c502b4819d0e39
Vali Maris <marisvali@yahoo.com>
2024-11-02 15:09:14 +0200

    just replace words with random words based on their frequency in English

--- hash.py
+++ hash.py
@@ -4,40 +4,29 @@ import string
 class TextHasher:
     def __init__(self):
         # Load dictionary.
-        with open('words_alpha.txt') as file:
-            self.all_english_words = [line.strip() for line in file]
-
-        # Create buckets of words of similar length (+/-2 chars).
-        self.english_words_of_length: dict[int, list[str]] = {}
-        for word in self.all_english_words:
-            fixed_length = len(word)
-            # Add to all buckets where it belongs.
-            for var_length in range(fixed_length-2, fixed_length+2):
-                # Protect against things like fixed_length == 1.
-                if var_length > 0:
-                    # Create list if it was never created.
-                    if var_length not in self.english_words_of_length.keys():
-                        self.english_words_of_length[var_length] = []
-                    # Add word to this list.
-                    self.english_words_of_length[var_length].append(word)
+        self.words : list[str] = []
+        self.weights : list[int] = []
+        with open('words_frequencies.csv') as file:
+            for line in file:
+                tokens = line.split(',')
+                self.words.append(tokens[0])
+                self.weights.append(int(tokens[1]))
 
     def _hashed_word(self, word: str) -> str:
         if len(word) == 0:
             return word
-        # Choose a random word of similar length.
-        # We might encounter words that are too long and don't have a bucket.
-        # In that case, map them to a random word.
-        length = len(word)
-        if length not in self.english_words_of_length.keys():
-            return random.choice(self.all_english_words)
-        else:
-            return random.choice(self.english_words_of_length[length])
+        # Choose a random word based on its frequency as used in the English language.
+        # This creates random speech that retains only the number of words from the original text.
+        # If the original text uses natural language and is large enough, the randomly generated
+        # text should exhibit some properties of the original text, such as:
+        # - similar overall text size
+        # - similar number of times a random part of the text is found throughout the whole text
+        return random.choices(population=self.words, weights=self.weights, k=1)[0]
 
     def hashed_text(self, text: str) -> str:
         # Go through the string. Add separators to the hashed text as they are.
         # Transform words between separators by hashing them and then add them to the hashed text.
         # We consider any non-letter to be a separator. Separators are ignored.
-        # The purpose is only to make the original meaning of the text impenetrable.
         h_text = ""
         word = ""
         for c in text:
